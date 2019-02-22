da46da7cb884088effc109f913d50350f5bcd478
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-02-22 09:37:21 +0100

    form af

--- register_form.php
+++ register_form.php
@@ -6,22 +6,14 @@
 <div class="row">
   <div class="col-6">
     <!-- Formulier -->
-    <form>
+    <form action="">
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-          placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <label for="exampleInputEmail1">E-mailadres</label>
+        <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp"
+          placeholder="Voer uw e-mailadres in...">
+        <small id="emailHelp" class="form-text text-muted">Wij zullen nooit uw e-mailadres delen met anderen.</small>
       </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Sla op!</button>
     </form>
   </div>
   <div class="col-6">
