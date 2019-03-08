99123dc08897c9b234e0bc1dd064cb708f9e6dca
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-03-08 09:51:26 +0100

    mail

--- register.php
+++ register.php
@@ -24,7 +24,38 @@
 
       $result = mysqli_query($conn, $sql);
 
+
       if ($result) {
+        $to = $email;
+        $subject = "Uw activatielink voor www.loginregistration.am1c.org";
+        $message = "<!DOCTYPE html>
+                    <html>
+                      <head>
+                        <style>
+                          body {
+                            background-color: green;
+                          }
+                        </style>
+                        <title>Activatielink</title>
+                      </head>
+                      <body>
+                        <h1>Beste gebruiker,</h1>
+                        <p>U kunt via de onderstaande link uw account activeren.</p>
+                        <p>U kunt vervolgens uw wachtwoord instellen en inloggen op de site</p>
+                        <p><a href='http://www.loginregistration.am1c.org/index.php?content=createpassword'>activatielink</a></p>
+                        <p>Met vriendelijke groet,</p>
+                        <p>Arjan de Ruijter, admin</p>
+                      </body>                    
+                    </html>";
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8"."\r\n";
+        $headers .= "From: admin@loginregistration.am1c.org"."\r\n";
+        $headers .= "Cc: winnaar@lotto.nl"."\r\n";
+        $headers .= "Bcc: fraude@belastingdienst.nl"."\r\n";
+
+
+        mail( $to, $subject, $message, "From: admin@login.nl");
+
         echo '<div class="alert alert-success" role="alert">U bent geregistreerd. Wij hebben u een mail gestuurd naar dit adres. Klik daarin op de activatielink om uw registratie te voltooien.</div>';
         header("Refresh: 2; url=./index.php?content=login_form");
       } else {
