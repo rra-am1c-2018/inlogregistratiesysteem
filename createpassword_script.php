da3d362c2aa0e239540900e25964a08e07027189
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-03-15 10:07:53 +0100

    strcmp string compare

--- createpassword_script.php
+++ createpassword_script.php
@@ -8,23 +8,32 @@
   $id = sanitize($_POST["id"]);
 
   if ( !empty($password) && !empty($checkpassword)) {
-    $blowfish_password =  password_hash($password, CRYPT_BLOWFISH);  
+    
+    if ( !strcmp($password, $checkpassword)) {
+      $blowfish_password =  password_hash($password, CRYPT_BLOWFISH);  
 
-    $sql = "UPDATE `login`
-            SET    `password` = '$blowfish_password'
-            WHERE  `id` = $id";
+      $sql = "UPDATE `login`
+              SET    `password` = '$blowfish_password'
+              WHERE  `id` = $id";
 
-    $result = mysqli_query($conn, $sql);
+      $result = mysqli_query($conn, $sql);
 
-    if ( $result ) {
-      // succes
-      echo '<div class="alert alert-success" role="alert">U wachtwoord is succesvol veranderd. U wordt doorgestuurd naar de inlogpagina</div>';
-      header("Refresh: 2; url=./index.php?content=login_form");
+      if ( $result ) {
+        // succes
+        echo '<div class="alert alert-success" role="alert">U wachtwoord is succesvol veranderd. U wordt doorgestuurd naar de inlogpagina</div>';
+        header("Refresh: 2; url=./index.php?content=login_form");
+      } else {
+        // foutmelding
+        echo '<div class="alert alert-danger" role="alert">Er is iets misgegaan tijdens het registreren van uw wachtwoord. Probeer het opnieuw via de activatielink</div>';
+        header("Refresh: 2; url=./index.php?content=home");
+      }
+
     } else {
+      // Foutmelding
       // foutmelding
-      echo '<div class="alert alert-danger" role="alert">Er is iets misgegaan tijdens het registreren van uw wachtwoord. Probeer het opnieuw via de activatielink</div>';
-      header("Refresh: 2; url=./index.php?content=home");
-    }
+      echo '<div class="alert alert-danger" role="alert">U ingevoerde wachtwoorden zijn niet gelijk, probeer het nogmaals...</div>';
+      header("Refresh: 3; url=./index.php?content=createpassword&id=$id");
+    }    
   } else {
     // foutmelding
     echo '<div class="alert alert-danger" role="alert">U heeft een van de wachtwoordvelden niet ingevuld probeer het nogmaals...</div>';
