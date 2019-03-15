2b416f5dd0c057e59b00e39f6b6ef1fbae4fe159
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-03-15 09:51:48 +0100

    empty

--- createpassword.php
+++ createpassword.php
@@ -8,11 +8,11 @@
     <form action="./index.php?content=createpassword_script" method="post">
       <div class="form-group">
         <label for="InputPassword1">Wachtwoord</label>
-        <input type="password" class="form-control" id="InputPassword1" placeholder="Kies een wachtwoord" name="password">
+        <input type="password" class="form-control" id="InputPassword1" placeholder="Kies een wachtwoord" name="password" required>
       </div>
       <div class="form-group">
         <label for="InputPassword">Herhaal wachtwoord</label>
-        <input type="password" class="form-control" id="InputPassword2" placeholder="Herhaal wachtwoord" name="checkpassword">
+        <input type="password" class="form-control" id="InputPassword2" placeholder="Herhaal wachtwoord" name="checkpassword" required>
       </div>
       <input type="hidden" value="<?php echo $_GET["id"]; ?>" name="id">
       <button type="submit" class="btn btn-primary btn-lg btn-block">Sla op!</button>
